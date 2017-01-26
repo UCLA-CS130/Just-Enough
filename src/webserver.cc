d2c56e24858d237c1c386cca4aa4adb4a2808fe4
Evan Krause <evangrayk@gmail.com>
2017-01-25 21:06:17 -0800

    read http request from socket

--- src/webserver.cc
+++ src/webserver.cc
@@ -1,9 +1,43 @@
 #include <string>
+#include <sstream>
 #include <boost/system/error_code.hpp>
 
 #include "webserver.h"
 
 using boost::asio::ip::tcp;
+
+std::string processRawRequest(std::string& reqStr) {
+    std::string response = reqStr;
+    return response;
+}
+
+void processConnection(tcp::socket& socket) {
+    std::cout << "Accepted connection from "
+        << socket.remote_endpoint().address().to_string()
+        << ":" << socket.remote_endpoint().port()
+        << std::endl;
+
+    boost::asio::streambuf buf;
+    std::string req;
+    while (true) {
+        boost::system::error_code err;
+        size_t bytes_read = read_until(socket, buf, "\r\n", err);
+
+        std::istream input(&buf);
+        std::string line(bytes_read, ' ');
+        input.read(&line[0], bytes_read);
+        req += line;
+
+        if (line.size() <= 2) {
+            break;
+        }
+
+    }
+    std::string response = processRawRequest(req);
+
+    boost::system::error_code ignored_error;
+    boost::asio::write(socket, boost::asio::buffer(response), ignored_error);
+}
 
 void Webserver::run() {
     try {
@@ -14,15 +48,8 @@ void Webserver::run() {
         while (true) {
             tcp::socket socket(io_service_);
             acceptor.accept(socket);
-            std::cout << "Accepted connection from "
-                << socket.remote_endpoint().address().to_string()
-                << ":" << socket.remote_endpoint().port()
-                << std::endl;
 
-            std::string msg = "Yes, hello\n";
-
-            boost::system::error_code ignored_error;
-            boost::asio::write(socket, boost::asio::buffer(msg), ignored_error);
+            processConnection(socket);
         }
     } catch (std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
