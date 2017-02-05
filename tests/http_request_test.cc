0a01be4d0d518106ab809d964e3bbb680e586a12
Evan Krause <evangrayk@gmail.com>
2017-02-04 21:33:10 -0800

    add tests for http_request

--- tests/http_request_test.cc
+++ tests/http_request_test.cc
@@ -46,6 +46,59 @@ TEST(HTTPRequestTest, loadFromRawRequest_Malformed) {
     EXPECT_EQ(err, HTTPRequestError_Malformed);
 }
 
+TEST(HTTPRequestTest, loadFromRawRequest_BadHTTP) {
+    std::string reqStr = (
+            "GET / HTP/1.1\r\n"
+            "User-Agent: Mozilla/1.0\r\n"
+            "\r\n"
+            );
+
+    HTTPRequest req;
+    HTTPRequestError err = req.loadFromRawRequest(reqStr);
+    EXPECT_EQ(err, HTTPRequestError_Malformed);
+}
+
+TEST(HTTPRequestTest, loadFromRawRequest_BadVersion) {
+    std::string reqStr = (
+            "GET / HTTP/2.0\r\n"
+            "User-Agent: Mozilla/1.0\r\n"
+            "\r\n"
+            );
+
+    HTTPRequest req;
+    HTTPRequestError err = req.loadFromRawRequest(reqStr);
+    EXPECT_EQ(err, HTTPRequestError_BadVersion);
+}
+
+TEST(HTTPRequestTest, loadFromRawRequest_POST) {
+    std::string reqStr = (
+            "POST / HTTP/1.1\r\n"
+            "User-Agent: Mozilla/1.0\r\n"
+            "\r\n"
+            );
+
+    HTTPRequest req;
+    HTTPRequestError err = req.loadFromRawRequest(reqStr);
+    EXPECT_EQ(err, HTTPRequestError_None);
+
+    EXPECT_EQ(req.getVerb(), HTTPVerb_POST);
+    EXPECT_EQ(req.getPath(), "/");
+    EXPECT_EQ(req.getHeader("User-Agent"), "Mozilla/1.0");
+}
+
+TEST(HTTPRequestTest, loadFromRawRequest_BadHeaders) {
+    std::string reqStr = (
+            "POST / HTTP/1.1\r\n"
+            "User-Agent: Mozilla/1.0\r\n"
+            "foo\r\n"
+            "\r\n"
+            );
+
+    HTTPRequest req;
+    HTTPRequestError err = req.loadFromRawRequest(reqStr);
+    EXPECT_EQ(err, HTTPRequestError_Malformed);
+}
+
 TEST(HTTPRequestTest, loadFromRawRequest_SomePath) {
     std::string reqStr = (
             "GET /some/path/to/file.jpg HTTP/1.1\r\n"
