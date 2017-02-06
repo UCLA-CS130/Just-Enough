e8396da274e21a28d6bd8b26a07919aeb7b8f0d2
joeydlee95 <joeydonglee@gmail.com>
2017-02-05 23:24:12 -0800

    refactored magic numbers

--- src/options.cc
+++ src/options.cc
@@ -3,6 +3,14 @@
 
 #include "options.h"
 
+const int MAX_PORT = 65535;
+const int MIN_PORT = 1024;
+
+//TODO: A better styling of name might be better here
+//TODO: Might also need to understand what = is
+const int COM = 0;
+const int ANS = 1;
+
 bool Options::loadOptionsFromStream(std::istream* config_file) {
 
 	NginxConfigParser parser;
@@ -16,12 +24,12 @@ bool Options::loadOptionsFromStream(std::istream* config_file) {
 	for (unsigned int i =0; i < config.statements_.size();i++) {
 
 		// Sets the port
-		if (!issetPort && config.statements_[i]->tokens_[0] == "server") {
+		if (!issetPort && config.statements_[i]->tokens_[COM] == "server") {
 			std::shared_ptr<NginxConfigStatement> temp_config = config.statements_[i];
 			for (unsigned int j = 0; j < temp_config->child_block_->statements_.size(); j++) {
-				if(temp_config->child_block_->statements_[j]->tokens_[0] == "listen") {
-					std::string port = temp_config->child_block_->statements_[j]->tokens_[1];
-					if((unsigned int) std::stoi(port) > 65535 || (unsigned int) std::stoi(port) < 1024) {
+				if(temp_config->child_block_->statements_[j]->tokens_[COM] == "listen") {
+					std::string port = temp_config->child_block_->statements_[j]->tokens_[ANS];
+					if((unsigned int) std::stoi(port) > MAX_PORT || (unsigned int) std::stoi(port) < MIN_PORT) {
 						std::cerr << "Invalid port number.\n";
         				return false;
 					}
