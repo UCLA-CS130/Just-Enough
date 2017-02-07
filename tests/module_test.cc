12f02fe3fc6aff65009610754791b93cad43374e
Evan Krause <evangrayk@gmail.com>
2017-02-06 22:01:23 -0800

    add module and echo module tests

---
+++ tests/module_test.cc
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+#include "module.h"
+#include <map>
+#include <memory>
+#include <string>
+
+using std::map;
+using std::string;
+
+TEST(ModuleTest, createModule_badType) {
+    auto paramMap = std::make_shared<map<string, string>>(
+            std::initializer_list<map<string, string>::value_type>{
+            {"type", "definitelyNotARealModuleType"},
+            {"path", "/foo"},
+            });
+
+    Module* mod = createModuleFromParameters(paramMap);
+    ASSERT_EQ(mod, nullptr);
+}
+
+TEST(ModuleTest, createModule_noType) {
+    auto paramMap = std::make_shared<map<string, string>>(
+            std::initializer_list<map<string, string>::value_type>{
+            {"path", "/foo"},
+            });
+
+    Module* mod = createModuleFromParameters(paramMap);
+    ASSERT_EQ(mod, nullptr);
+}
+
+TEST(ModuleTest, createModule_noPath) {
+    auto paramMap = std::make_shared<map<string, string>>(
+            std::initializer_list<map<string, string>::value_type>{
+            {"type", "definitelyNotARealModuleType"},
+            });
+
+    Module* mod = createModuleFromParameters(paramMap);
+    ASSERT_EQ(mod, nullptr);
+}
