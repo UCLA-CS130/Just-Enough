f33b50b19dd566bd7a5dd89d6508f0fc77cbedc7
joeydlee95 <joeydonglee@gmail.com>
2017-02-06 17:01:12 -0800

    accessor method for modules

--- src/module.cc
+++ src/module.cc
@@ -6,12 +6,13 @@
 
 //const std::string firstEntry[3] = {"type", "path", "filebase"};
 
+//Easier way to program this?
 Module::Module() {
 	std::map<std::string, std::string> moduleParameters;
 }
 
 bool Module::add(const std::string& str, const std::string& str2) {
-	if(str == "" || str2 == "") {
+	if(str.empty() || str2.empty()) {
 		std::cerr << "Entries to module are empty.\n";
 		return false;
 	}
@@ -26,14 +27,17 @@ bool Module::add(const std::string& str, const std::string& str2) {
 		moduleParameters[str] = str2;
 		return true;
 	}
-	
+
 	std::cerr << "Failed to add to module map.\n";
 	return false;
 }
 
-/*std::string Module::getPathName(const std::string str) {
-	auto it = moduleParameters.find(str);
-	if(it != moduleParameters.end()) {
-		return it->second;
+const std::string Module::getPathName(const std::string& str) {
+	std::map<std::string, std::string>::iterator it;
+	it = moduleParameters.find(str);
+	if(it == moduleParameters.end()) {
+		std::cerr << "Could not find particular path in module map.\n";
+		return "";
 	}
-}*/
+	return it->second;
+}
--- src/module.h
+++ src/module.h
@@ -6,7 +6,7 @@ class Module {
 	public:
 		Module();
 		bool add(const std::string& str, const std::string& str2);
-		//const std::string getPathName(const std::string str);
+		const std::string getPathName(const std::string& str);
 
 	private:
 		std::map<std::string, std::string> moduleParameters;
