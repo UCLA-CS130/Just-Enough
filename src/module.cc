568024caa5a860b3d510afb2fa54cc57599beaed
Evan Krause <evangrayk@gmail.com>
2017-02-07 19:06:52 -0800

    add static file serving module

--- src/module.cc
+++ src/module.cc
@@ -4,6 +4,7 @@
 
 #include "module.h"
 #include "echo_module.h"
+#include "static_module.h"
 
 Module* createModuleFromParameters(std::shared_ptr<std::map<std::string, std::string>> params) {
     auto typeParam = params->find("type");
@@ -20,6 +21,9 @@ Module* createModuleFromParameters(std::shared_ptr<std::map<std::string, std::st
 
     if (typeParam->second == EchoModule::typeString) {
         Module* mod = EchoModule::createFromParameters(pathParam->second, params);
+        return mod;
+    } else if (typeParam->second == StaticModule::typeString) {
+        Module* mod = StaticModule::createFromParameters(params);
         return mod;
     } else {
         std::cerr << "Unknown module type \"" << typeParam->second << "\"." << std::endl;
