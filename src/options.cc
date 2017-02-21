85d8a628b9d7420d0aa0da0569fc6e80cdd24177
joeydlee95 <joeydonglee@gmail.com>
2017-02-21 02:52:37 -0800

    Changes for config for Webserver API

--- src/options.cc
+++ src/options.cc
@@ -13,17 +13,27 @@ const int MIN_PORT = 1024;
 
 const int KEY = 0;
 const int VAL = 1;
+const int HANDLER = 3;
 
 const int STATEMENT_SIZE = 2;
+const int PATH_SIZE = 3;
 
 bool Options::addModule(std::shared_ptr<NginxConfigStatement> module_config) {
     std::shared_ptr<stringMap> params = std::make_shared<stringMap>();
 
+    // Get path first.
+    if(module_config->tokens_.size() != PATH_SIZE) {
+        std::cerr << "Not a valid path statement. Need three tokens.\n";
+        return false;
+    }
+    string location = module_config->tokens_[VAL];
+    string handlerType = module_config->tokens_[HANDLER];
+
+    // Enter child block.
     for(size_t i = 0; i < module_config->child_block_->statements_.size(); i++) {
 
-        // For now, not allowed to go into another module/server within a module
         if(module_config->child_block_->statements_[i]->tokens_.size() != STATEMENT_SIZE) {
-            std::cerr << "Not a valid module statement. Need two tokens.\n";
+            std::cerr << "Not a valid child block statement. Need two tokens.\n";
             return false;
         }
         string firstStatement = module_config->child_block_->statements_[i]->tokens_[KEY];
@@ -34,7 +44,6 @@ bool Options::addModule(std::shared_ptr<NginxConfigStatement> module_config) {
             // remove surrounding quotes
             secondStatement = secondStatement.substr(1, secondStatement.size()-2);
         }
-
         params->insert(std::pair<string,string>(firstStatement, secondStatement));
     }
 
@@ -72,35 +81,31 @@ bool Options::loadOptionsFromStream(std::istream* config_file) {
 
     bool issetPort = false;
     for (unsigned int i =0; i < config.statements_.size(); i++) {
-        if (config.statements_[i]->tokens_[KEY] == "server") {
-            std::shared_ptr<NginxConfigStatement> temp_config = config.statements_[i];
-            for (unsigned int j = 0; j < temp_config->child_block_->statements_.size(); j++) {
+        std::shared_ptr<NginxConfigStatement> temp_config = config.statements_[i];
 
-                // Sets port.
-                if(temp_config->child_block_->statements_[j]->tokens_[KEY] == "port") {
-                    if(issetPort) {
-                        std::cerr << "Multiple ports in config file.\n";
-                        return false;
-                    } else {
-                        issetPort = addPort(temp_config->child_block_->statements_[j]);
-                        if(!issetPort) {
-                            return false;
-                        }
-                    }
-                }
-
-                // Sets modules.
-                else if (temp_config->child_block_->statements_[j]->tokens_[KEY] == "module"){
-                    if(addModule(temp_config->child_block_->statements_[j]) == false) {
-                        return false;
-                    }
-                }
-
-                // For future child blocks in config file.
-                else{
+        // Sets port.
+        if(temp_config->tokens_[KEY] == "port") {
+            if(issetPort) {
+                std::cerr << "Multiple ports in config file.\n";
+                    return false;
+            } else {
+                issetPort = addPort(temp_config);
+                if(!issetPort) {
                     return false;
                 }
             }
+        }
+
+        // Sets path.
+        else if (temp_config->tokens_[KEY] == "path"){
+            if(addModule(temp_config) == false) {
+                return false;
+            }
+        }
+
+        // For future statements in config file.
+        else {
+            return false;
         }
     }
     return true;
