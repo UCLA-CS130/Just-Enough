b587739c6b0c1692b607424404fcc9c1228e819c
joeydlee95 <joeydonglee@gmail.com>
2017-02-23 01:35:34 -0800

    Changes to options struct with handlers patch

--- src/options.h
+++ src/options.h
@@ -8,10 +8,7 @@
 
 struct Options {
 	unsigned short port;
-	std::vector<Module*> modules;
 	std::map<std::string, RequestHandler*> handlerMap;
-	bool addModuleToVec(Module* module);
-	bool addModule(std::shared_ptr<NginxConfigStatement> module_config);
 	bool addHandler(std::shared_ptr<NginxConfigStatement> handler_config);
 	bool addPort(std::shared_ptr<NginxConfigStatement> port_config);
 	bool loadOptionsFromStream(std::istream* config_file);
