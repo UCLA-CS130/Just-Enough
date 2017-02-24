9d0cf08b8333b0f4a3652185964e98274fa249cc
Evan Krause <evangrayk@gmail.com>
2017-02-23 23:14:52 -0800

    add multimap counter

---
+++ tests/multimap_counter_test.cc
@@ -0,0 +1,127 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "multimap_counter.h"
+#include <sstream>
+#include <thread>
+
+TEST(MultimapCounterTest, basic) {
+    MultiMapCounter<std::string, int> mm_counter;
+
+    mm_counter.increment("foo", 1);
+    mm_counter.increment("foo", 1);
+    mm_counter.increment("foo", 1);
+
+    mm_counter.increment("foo", 2);
+    mm_counter.increment("foo", 2);
+
+    mm_counter.increment("bar", 1);
+    mm_counter.increment("bar", 2);
+    mm_counter.increment("bar", 3);
+
+    mm_counter.increment("baz", 5);
+
+    EXPECT_EQ(mm_counter.get("foo", 1), 3);
+    EXPECT_EQ(mm_counter.get("foo", 2), 2);
+    EXPECT_EQ(mm_counter.get("foo", 3), 0);
+
+    EXPECT_EQ(mm_counter.get("bar", 1), 1);
+    EXPECT_EQ(mm_counter.get("bar", 2), 1);
+    EXPECT_EQ(mm_counter.get("bar", 3), 1);
+
+    EXPECT_EQ(mm_counter.get("baz", 1), 0);
+    EXPECT_EQ(mm_counter.get("baz", 5), 1);
+}
+
+TEST(MultimapCounterTest, largerIncrement) {
+    MultiMapCounter<std::string, int> mm_counter;
+
+    mm_counter.increment("foo", 1, 5);
+    mm_counter.increment("foo", 1);
+    mm_counter.increment("foo", 1);
+
+    mm_counter.increment("bar", 4, 100);
+
+    EXPECT_EQ(mm_counter.get("foo", 1), 7);
+    EXPECT_EQ(mm_counter.get("foo", 2), 0);
+
+    EXPECT_EQ(mm_counter.get("bar", 4), 100);
+}
+
+TEST(MultimapCounterTest, iterateInner) {
+    MultiMapCounter<std::string, int> mm_counter;
+
+    mm_counter.increment("foo", 1);
+    mm_counter.increment("foo", 1);
+    mm_counter.increment("foo", 2);
+    mm_counter.increment("foo", 3);
+
+    for (auto p : mm_counter.iterateKey("foo")) {
+        switch (p.first) {
+            case 1:
+                EXPECT_EQ(p.second, 2);
+                break;
+            case 2:
+                EXPECT_EQ(p.second, 1);
+                break;
+            case 3:
+                EXPECT_EQ(p.second, 1);
+                break;
+            default:
+                EXPECT_TRUE(false);
+                break;
+        }
+    }
+}
+
+TEST(MultimapCounterTest, theadedTest) {
+    MultiMapCounter<std::string, int> mm_counter;
+
+    int n = 100;
+
+    // run several threads accessing same map
+    std::thread t1([&]() {
+            for (int i = 0; i < n; i++) {
+                mm_counter.increment("foo", 1);
+            }
+            });
+    std::thread t2([&]() {
+            for (int i = 0; i < n; i++) {
+                mm_counter.increment("foo", 1);
+            }
+            });
+    std::thread t3([&]() {
+            for (int i = 0; i < n; i++) {
+                mm_counter.increment("foo", 1);
+            }
+            });
+    std::thread t4([&]() {
+            for (int i = 0; i < n; i++) {
+                mm_counter.increment("bar", 5);
+            }
+            });
+    std::thread t5([&]() {
+            for (int i = 0; i < n; i++) {
+                mm_counter.increment("bar", 5);
+            }
+            });
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+
+    for (auto p : mm_counter.iterateKey("foo")) {
+        switch (p.first) {
+            case 1:
+                EXPECT_EQ(p.second, 3*n*1);
+                break;
+            case 2:
+                EXPECT_EQ(p.second, 2*n*5);
+                break;
+            default:
+                EXPECT_TRUE(false);
+                break;
+        }
+    }
+}
