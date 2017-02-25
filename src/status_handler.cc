7feaf42fd99ebb31d1d1ee228c7a12a077505fab
Evan Krause <evangrayk@gmail.com>
2017-02-24 18:53:35 -0800

    show default handler in status page

--- src/status_handler.cc
+++ src/status_handler.cc
@@ -19,6 +19,7 @@ RequestHandler::Status StatusHandler::HandleRequest(const Request& req, Response
     for (auto h : opt->handlerMap) {
         handlersStr += "<tr><td>" + h.first + "</td><td>" + h.second->type() + "</td></tr>";
     }
+    handlersStr += "<tr><td>(default)</td><td>" + opt->defaultHandler->type() + "</td></tr>";
 
     std::string requestCountsStr;
     auto counters = ws->counters();
