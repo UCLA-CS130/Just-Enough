b3be44d887a85491c738684aecaddefcffff2748
joeydlee95 <joeydonglee@gmail.com>
2017-03-12 14:21:38 -0700

    working static readme.md

--- src/static_handler.cc
+++ src/static_handler.cc
@@ -8,29 +8,37 @@ using std::string;
 size_t STATEMENT_SIZE = 2;
 
 RequestHandler::Status StaticHandler::Init(const std::string& uri_prefix, const NginxConfig& config) {
-  path_ = uri_prefix;
-  for(size_t i = 0; i < config.statements_.size(); i++) {
-    if (config.statements_[i]->tokens_.size() == STATEMENT_SIZE){
-        if(config.statements_[i]->tokens_[0] == "root"){
-          filebase_ = config.statements_[i]->tokens_[1];
-        }
-        else{
-          std::cerr << "Invalid Static Handler 'filebase' parameter." << std::endl;
-          return RequestHandler::Error;
+    path_ = uri_prefix;
+    bool issetMarkdown = false;
+    for (size_t i = 0; i < config.statements_.size(); i++) {
+        if (config.statements_[i]->tokens_.size() == STATEMENT_SIZE) {
+            if (config.statements_[i]->tokens_[0] == "root") {
+                filebase_ = config.statements_[i]->tokens_[1];
+            } else if ( ! issetMarkdown && 
+                        config.statements_[i]->tokens_[0] == "convert_markdown") {
+                if (config.statements_[i]->tokens_[1] == "true") {
+                    convert_markdown_ = true;
+                    issetMarkdown = true;
+                } else {
+                    convert_markdown_ = false;
+                    issetMarkdown = true;
+                }
+            } else {
+                std::cerr << "Invalid Static Handler parameter." << std::endl;
+                return RequestHandler::Error;
+            }
+        } else {
+            std::cerr << "Invalid Static Handler config" << std::endl;
+            return RequestHandler::Error;
         }
     }
-    else{
-      std::cerr << "Invalid Static Handler config" << std::endl;
-      return RequestHandler::Error;
-    }
-  }
 
-  notFoundHandler_ = RequestHandler::CreateByName("NotFoundHandler");
-  if ( ! notFoundHandler_) {
-      std::cerr << "Static handler could not create NotFoundHandler." << std::endl;
-      return RequestHandler::Error;
-  }
-  return RequestHandler::OK;
+    notFoundHandler_ = RequestHandler::CreateByName("NotFoundHandler");
+    if ( ! notFoundHandler_) {
+        std::cerr << "Static handler could not create NotFoundHandler." << std::endl;
+        return RequestHandler::Error;
+    }
+    return RequestHandler::OK;
 }
 
 RequestHandler::Status StaticHandler::HandleRequest(const Request& req, Response* resp) {
@@ -74,21 +82,21 @@ RequestHandler::Status StaticHandler::HandleRequest(const Request& req, Response
 
             resp->SetStatus(Response::code_200_OK);
 
-            if (mt ==  MIMEType_md) {
+            if (convert_markdown_) {
                 string converted_md = convertMarkdownToHtml(data);
                 if (converted_md == "") {
-                    // something went wrong in converting the markdown
+                    // Something went wrong in converting the markdown.
                     resp->SetStatus(Response::code_500_internal_error);
                     return RequestHandler::OK;
                 }
                 resp->AddHeader("Content-Type", MIMETypeMap.at(MIMEType_html));
                 resp->SetBody(converted_md);
                 return RequestHandler::OK;
+            } else {
+                resp->AddHeader("Content-Type", contentType);
+                resp->SetBody(data);
+                return RequestHandler::OK;
             }
-
-            resp->AddHeader("Content-Type", contentType);
-            resp->SetBody(data);
-            return RequestHandler::OK;
     }
 
     resp->SetStatus(Response::code_500_internal_error);
--- src/static_handler.h
+++ src/static_handler.h
@@ -3,23 +3,24 @@
 
 class StaticHandler : public RequestHandler {
     public:
-      virtual Status Init(const std::string& uri_prefix,
+        virtual Status Init(const std::string& uri_prefix,
               const NginxConfig& config);
 
-      virtual Status HandleRequest(const Request& request,
+        virtual Status HandleRequest(const Request& request,
               Response* response);
 
-      virtual ~StaticHandler();
+        virtual ~StaticHandler();
 
-      virtual std::string type() const {
-          return "StaticHandler";
-      }
+        virtual std::string type() const {
+            return "StaticHandler";
+        }
 
 
     protected:
-      std::string filebase_;
-      std::string path_;
-      RequestHandler* notFoundHandler_;
+        std::string filebase_;
+        bool convert_markdown_;
+        std::string path_;
+        RequestHandler* notFoundHandler_;
 };
 
 REGISTER_REQUEST_HANDLER(StaticHandler);
